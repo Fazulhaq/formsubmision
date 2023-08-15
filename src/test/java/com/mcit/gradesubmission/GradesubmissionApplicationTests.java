8787a841a3db2bdc0a4d4543285c6f1689dbb804
Fazulhaq Amin <128462337+Fazulhaq@users.noreply.github.com>
2023-08-15 14:21:49 +0430

    Integration Test implemented on the project

--- src/test/java/com/mcit/gradesubmission/GradesubmissionApplicationTests.java
+++ src/test/java/com/mcit/gradesubmission/GradesubmissionApplicationTests.java
@@ -1,13 +1,66 @@
 package com.mcit.gradesubmission;
 
+import com.mcit.gradesubmission.controller.GradeController;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class GradesubmissionApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+		System.out.println(mockMvc);
+	}
+	@Test
+	public void testShowGradeForm() throws Exception {
+		RequestBuilder request = MockMvcRequestBuilders.get("/?id=123");
+
+		mockMvc.perform(request)
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(view().name("form"))
+				.andExpect(model().attributeExists("grade"));
+	}
+	@Test
+	public void testSuccessfullSubmision() throws Exception {
+		RequestBuilder request = MockMvcRequestBuilders.post("/handleSubmit")
+				.param("name","Ahmad")
+				.param("subject","Math")
+				.param("score","A-");
+
+		mockMvc.perform(request)
+				.andExpect(status().is3xxRedirection())
+				.andExpect(redirectedUrl("/grades"));
+	}
+	@Test
+	public void testUnsuccessfullSubmision() throws Exception {
+		RequestBuilder request = MockMvcRequestBuilders.post("/handleSubmit")
+				.param("name", "    ")
+				.param("subject","     ")
+				.param("score", "     ");
+
+		mockMvc.perform(request)
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(view().name("form"));
+	}
+	@Test
+	public void testGetGrades() throws Exception {
+		RequestBuilder request = MockMvcRequestBuilders.get("/grades");
+
+		mockMvc.perform(request)
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(view().name("grades"))
+				.andExpect(model().attributeExists("grades"));
 	}
 
 }
